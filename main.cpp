48c8f085e46c1266c8fc7d78a20e9a19373f1e8f
li95710222 <li95710222@gmail.com>
2024-06-06 20:39:02 +0800

    test1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    int a;
+    cin>>a;
+    cout<<a;
+    cout << "Hello, World!" << endl;
     return 0;
 }
